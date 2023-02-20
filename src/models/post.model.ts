358981b392d78550afd78ffbcf3f70b00f00428d
ChrisEs146 <chris.es146@gmail.com>
2023-02-20 11:41:50 -0600

    Add: Type error message to the description field

--- src/models/post.model.ts
+++ src/models/post.model.ts
@@ -7,6 +7,12 @@ const postSchema = z.object({
       invalid_type_error: "Title must be text(string)",
     })
     .trim(),
+  description: z
+    .string({
+      required_error: "Description is required",
+      invalid_type_error: "Description must be text(string)",
+    })
+    .trim(),
 });
 
 export type PostInput = z.infer<typeof postSchema>;
