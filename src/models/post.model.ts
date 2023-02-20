b069fa46a6b0448fbcf9a205c7e62284752ff5ea
ChrisEs146 <chris.es146@gmail.com>
2023-02-20 11:41:22 -0600

    Add: type error message to title field

--- src/models/post.model.ts
+++ src/models/post.model.ts
@@ -1,9 +1,12 @@
 import { z } from "zod";
 
 const postSchema = z.object({
-  title: z.string({ required_error: "Title is required" }).trim(),
-  description: z.string({ required_error: "Description is required" }).trim(),
-  image: z.string({ required_error: "Image is required" }).trim(),
+  title: z
+    .string({
+      required_error: "Title is required",
+      invalid_type_error: "Title must be text(string)",
+    })
+    .trim(),
 });
 
 export type PostInput = z.infer<typeof postSchema>;
