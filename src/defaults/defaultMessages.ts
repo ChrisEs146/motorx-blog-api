76b43a8f6f9028b97244e18a93f385c4af7afbfa
ChrisEs146 <chris.es146@gmail.com>
2023-02-23 17:36:27 -0600

    Add: defaultMessages object

---
+++ src/defaults/defaultMessages.ts
@@ -0,0 +1,12 @@
+export const errorMsgs = {
+  NotFound: "Resource was not found.",
+  Conflict: "Resource already exists.",
+  BadRequest: "Resource is not valid",
+};
+
+export const successMsgs = {
+  AccountOk: "Account created successfully.",
+  NameUpdateOk: "Name updated successfully.",
+  PassUpdateOk: "Password changed successfully",
+  PostOk: "Post created successfully.",
+};
