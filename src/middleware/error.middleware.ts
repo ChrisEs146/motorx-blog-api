092508c9c5fc19e5512e9a85490f1f90dcfe79f6
ChrisEs146 <chris.es146@gmail.com>
2023-02-23 17:37:11 -0600

    Add: Error handler for 404 erros

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -14,3 +14,16 @@ export function handleError(err: CustomError, _req: Request, res: Response): voi
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
 }
+
+/**
+ * Global error handler for 404 errors
+ * @param err Error object
+ * @param _req Request
+ * @param res Response
+ */
+export function handleNotFoundError(err: CustomError, _req: Request, res: Response): void {
+  res.status(400).json({
+    message: err.message,
+    stack: process.env.NODE_ENV === "production" ? null : err.stack,
+  });
+}
