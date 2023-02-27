8ec3fff8110afbb6f87296e29ebce364a67e78b6
ChrisEs146 <chris.es146@gmail.com>
2023-02-27 17:09:41 -0600

    update: errorMsgs

--- src/defaults/defaultMessages.ts
+++ src/defaults/defaultMessages.ts
@@ -2,6 +2,9 @@ export const errorMsgs = {
   NotFound: "Resource was not found.",
   Conflict: "Resource already exists.",
   BadRequest: "Resource is not valid",
+  NotMatch: "Passwords don't match",
+  UserError: "Something went wrong creating the user",
+  invalidCreds: "Email or password are incorrect",
 };
 
 export const successMsgs = {
