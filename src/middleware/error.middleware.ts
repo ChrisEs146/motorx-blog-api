c13d2378a762f193846e133d8a418532b3811a45
ChrisEs146 <chris.es146@gmail.com>
2023-02-27 17:10:01 -0600

    update: error handlers

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -1,5 +1,5 @@
 import { Request, Response } from "express";
-import { CustomError } from "./middleware.interfaces.js";
+import { ICustomError } from "./middleware.interfaces.js";
 
 /**
  * Global error handler
@@ -7,8 +7,8 @@ import { CustomError } from "./middleware.interfaces.js";
  * @param _req Request
  * @param res Response
  */
-export function handleError(err: CustomError, _req: Request, res: Response): void {
-  const status = err.statusCode || err.status || 500;
+export function handleError(err: ICustomError, _req: Request, res: Response): void {
+  const status = err.statusCode || 500;
   res.status(status).json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
@@ -21,7 +21,7 @@ export function handleError(err: CustomError, _req: Request, res: Response): voi
  * @param _req Request
  * @param res Response
  */
-export function handleNotFoundError(err: CustomError, _req: Request, res: Response): void {
+export function handleNotFoundError(err: ICustomError, _req: Request, res: Response): void {
   res.status(400).json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
