f49fdfc64f72fc1ae22208f500d9ce3f610d78f4
ChrisEs146 <chris.es146@gmail.com>
2023-02-27 17:11:41 -0600

    Add: custom Error class

---
+++ src/utils/customError.ts
@@ -0,0 +1,9 @@
+export class CustomError extends Error {
+  public statusCode: number;
+  public message: string;
+  constructor(statusCode: number, message: string) {
+    super();
+    this.statusCode = statusCode;
+    this.message = message;
+  }
+}
