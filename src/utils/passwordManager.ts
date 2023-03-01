8f740d9b39aac560614b533122f0853fa677b842
ChrisEs146 <chris.es146@gmail.com>
2023-03-01 17:23:02 -0600

    Add: js docs to functions

--- src/utils/passwordManager.ts
+++ src/utils/passwordManager.ts
@@ -1,7 +1,7 @@
 import bcrypt from "bcrypt";
 
 /**
- *
+ * Hashes a given password using the given amount of rounds.
  * @param rounds Number of rounds
  * @param password Password
  * @returns Hashed password
@@ -11,10 +11,15 @@ export async function hashPassword(rounds: number, password: string): Promise<st
     throw new Error("Must provide number of rounds and password");
   }
   const salt = await bcrypt.genSalt(rounds);
-  const finalPassword = await bcrypt.hash(password, salt);
-  return finalPassword;
+  return await bcrypt.hash(password, salt);
 }
 
+/**
+ * Validates if a given password matches the hashed password.
+ * @param normalPwd Plain text password
+ * @param hashedPwd Hashed password
+ * @returns boolean
+ */
 export async function comparePassword(normalPwd: string, hashedPwd: string): Promise<boolean> {
   if (!normalPwd || !hashedPwd) {
     throw new Error("Must provide normal password and hashed password");
