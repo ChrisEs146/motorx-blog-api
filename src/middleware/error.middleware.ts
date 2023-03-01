b993d49de1b9fb58bd98695c76ce0d505097cbd1
ChrisEs146 <chris.es146@gmail.com>
2023-03-01 17:07:29 -0600

    Update: handleError middleware

--- src/middleware/error.middleware.ts
+++ src/middleware/error.middleware.ts
@@ -7,9 +7,10 @@ import { ICustomError } from "./middleware.interfaces.js";
  * @param _req Request
  * @param res Response
  */
-export function handleError(err: ICustomError, _req: Request, res: Response): void {
-  const status = err.statusCode || 500;
-  res.status(status).json({
+export function handleError(err: ICustomError, _req: Request, res: Response) {
+  const status = err.statusCode ?? 500;
+  res.status(status);
+  return res.json({
     message: err.message,
     stack: process.env.NODE_ENV === "production" ? null : err.stack,
   });
