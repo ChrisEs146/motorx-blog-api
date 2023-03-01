79e955fdfc0335ff72405e343be3e6ea7ad237b9
ChrisEs146 <chris.es146@gmail.com>
2023-03-01 17:05:44 -0600

    update: jest config

--- jest.config.ts
+++ jest.config.ts
@@ -2,6 +2,11 @@ import { JestConfigWithTsJest } from "ts-jest";
 const jestConfig: JestConfigWithTsJest = {
   preset: "ts-jest",
   testEnvironment: "node",
+  verbose: true,
+  resolver: "ts-jest-resolver",
+  extensionsToTreatAsEsm: [".ts"],
+  moduleFileExtensions: ["ts", "js"],
+  moduleDirectories: ["node_modules", "<rootDir>"],
 };
 
 export default jestConfig;
