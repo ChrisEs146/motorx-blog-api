4653cfcac40b625ea3b80e22442b743ebcceaef3
ChrisEs146 <chris.es146@gmail.com>
2023-03-02 20:53:59 -0600

    Update: Convert default variables to string

--- src/defaults/defaultVariables.ts
+++ src/defaults/defaultVariables.ts
@@ -1,10 +1,10 @@
 const processVar = {
   PORT: Number(process.env.PORT),
-  NODE_ENV: process.env.NODE_ENV,
-  ACCESS_TOKEN_KEY: process.env.ACCESS_JWT_SECRET,
-  ACCESS_TOKEN_EXPIRY: process.env.ACCESS_JWT_EXPIRY,
-  REFRESH_TOKEN_KEY: process.env.REFRESH_TOKEN_SECRET,
-  REFRESH_TOKEN_EXPIRY: process.env.REFRESH_TOKEN_EXPIRY,
+  NODE_ENV: String(process.env.NODE_ENV),
+  ACCESS_TOKEN_KEY: String(process.env.ACCESS_JWT_SECRET),
+  ACCESS_TOKEN_EXPIRY: String(process.env.ACCESS_JWT_EXPIRY),
+  REFRESH_TOKEN_KEY: String(process.env.REFRESH_TOKEN_SECRET),
+  REFRESH_TOKEN_EXPIRY: String(process.env.REFRESH_TOKEN_EXPIRY),
 };
 
 export default processVar;
