6ab6903b7ef6416314892dd7fb5604ade5c9c115
ChrisEs146 <chris.es146@gmail.com>
2023-03-03 18:27:00 -0600

    Update: CustomError

--- src/utils/customError.ts
+++ src/utils/customError.ts
@@ -2,11 +2,14 @@
  * Custom error class
  */
 export class CustomError extends Error {
-  public statusCode: number;
+  public statusCode?: number;
+  public status?: number;
   public message: string;
-  constructor(statusCode: number, message: string) {
-    super();
+  public error: string | null;
+  constructor(statusCode: number, message: string, error?: string) {
+    super(message);
     this.statusCode = statusCode;
     this.message = message;
+    this.error = error || null;
   }
 }
