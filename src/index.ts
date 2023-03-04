1c4c786532d2cf04554c49da73d5467845d57c33
ChrisEs146 <chris.es146@gmail.com>
2023-03-03 18:24:17 -0600

    Add: return type

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ import app from "./app.js";
 import processVar from "./defaults/defaultVariables.js";
 
 const PORT = processVar.PORT || 8000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, (): void => console.log(`Server running on port ${PORT}`));
