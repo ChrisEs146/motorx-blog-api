74cd6222d4d1fbba8ba78bae69f08fca093cec15
ChrisEs146 <chris.es146@gmail.com>
2023-03-10 18:45:57 -0600

    Update: spread body and params

--- src/middleware/validation.middleware.ts
+++ src/middleware/validation.middleware.ts
@@ -4,7 +4,7 @@ import { AnyZodObject, ZodError } from "zod";
 function validateResource(customSchema: AnyZodObject) {
   return (req: Request, res: Response, next: NextFunction): void => {
     try {
-      customSchema.parse({ body: req.body as unknown, params: req.params });
+      customSchema.parse({ ...req.body, ...req.params });
       next();
     } catch (error: unknown) {
       res.status(400).json(error);
