82e4872ef97ee18beebfdc9abbe06d1abfaa66fe
ChrisEs146 <chris.es146@gmail.com>
2023-03-10 18:40:17 -0600

    Add: resetEmail and forgotPassword schema

--- src/schemas/user.types.ts
+++ src/schemas/user.types.ts
@@ -4,6 +4,8 @@ import {
   updatePasswordSchema,
   updateUserSchema,
   logInUserSchema,
+  resetEmailSchema,
+  forgotPasswordSchema,
 } from "./user.schema.js";
 
 // User input types
@@ -11,3 +13,5 @@ export type CreateUserInput = z.infer<typeof createUserSchema>;
 export type UpdateUserInput = z.infer<typeof updateUserSchema>;
 export type UpdatePasswordInput = z.infer<typeof updatePasswordSchema>;
 export type LogInUserInput = z.infer<typeof logInUserSchema>;
+export type ResetEmailInput = z.infer<typeof resetEmailSchema>;
+export type ForgotPasswordInput = z.infer<typeof forgotPasswordSchema>;
