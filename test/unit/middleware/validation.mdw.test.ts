d964e3680527dd8c85cbfd57ef5c6d88eb40840e
ChrisEs146 <chris.es146@gmail.com>
2023-03-10 18:50:32 -0600

    Add: checks validation errors in user creation

--- test/unit/middleware/validation.mdw.test.ts
+++ test/unit/middleware/validation.mdw.test.ts
@@ -93,6 +93,126 @@ describe("Validation middleware", () => {
   });
 });
 
+describe("validate create user schema", () => {
+  let mockReq: Partial<Request>;
+  let mockRes: Partial<Response>;
+  let next: NextFunction;
+
+  beforeEach(() => {
+    mockReq = {};
+    mockRes = {
+      status: jest.fn().mockReturnThis(),
+      json: jest.fn(),
+    };
+    next = jest.fn();
+  });
+
+  it("Must throw error first name is required", () => {
+    const invalidBody = {
+      firstName: undefined,
+      lastName: "Morgan",
+      email: "arthur@email.com",
+      password: "testPassword",
+      confirmPassword: "testPassword",
+    };
+    mockReq = { body: invalidBody };
+    const expectedRes = {
+      message: "First name is required",
+    };
+    const testFunc = validateResource(createUserSchema);
+    testFunc(mockReq as Request, mockRes as Response, next);
+    expect(mockRes.json).toBeCalledWith(expectedRes);
+    expect(mockRes.status).toBeCalledWith(400);
+  });
+
+  it("Must throw error last name must be string", () => {
+    const invalidBody = {
+      firstName: "Arthur",
+      lastName: 24,
+      email: "arthur@email.com",
+      password: "testPassword",
+      confirmPassword: "testPassword",
+    };
+    mockReq = { body: invalidBody };
+    const expectedRes = {
+      message: "Last name must be a string",
+    };
+    const testFunc = validateResource(createUserSchema);
+    testFunc(mockReq as Request, mockRes as Response, next);
+    expect(mockRes.json).toBeCalledWith(expectedRes);
+    expect(mockRes.status).toBeCalledWith(400);
+  });
+
+  it("Must throw error invalid email", () => {
+    const invalidBody = {
+      firstName: "Arthur",
+      lastName: "Morgan",
+      email: "arthur@",
+      password: "testPassword",
+      confirmPassword: "testPassword",
+    };
+    mockReq = { body: invalidBody };
+    const expectedRes = {
+      message: "Invalid email address",
+    };
+    const testFunc = validateResource(createUserSchema);
+    testFunc(mockReq as Request, mockRes as Response, next);
+    expect(mockRes.json).toBeCalledWith(expectedRes);
+    expect(mockRes.status).toBeCalledWith(400);
+  });
+
+  it("Must throw error password at least 8 chars long", () => {
+    const invalidBody = {
+      firstName: "Arthur",
+      lastName: "Morgan",
+      email: "arthur@email.com",
+      password: "test",
+      confirmPassword: "testPassword",
+    };
+    mockReq = { body: invalidBody };
+    const expectedRes = {
+      message: "Password must be at least 8 chars long",
+    };
+    const testFunc = validateResource(createUserSchema);
+    testFunc(mockReq as Request, mockRes as Response, next);
+    expect(mockRes.json).toBeCalledWith(expectedRes);
+    expect(mockRes.status).toBeCalledWith(400);
+  });
+
+  it("Must throw error confirm password is required", () => {
+    const invalidBody = {
+      firstName: "Arthur",
+      lastName: "Morgan",
+      email: "arthur@email.com",
+      password: "testPassword",
+      confirmPassword: undefined,
+    };
+    mockReq = { body: invalidBody };
+    const expectedRes = {
+      message: "Confirm password is required",
+    };
+    const testFunc = validateResource(createUserSchema);
+    testFunc(mockReq as Request, mockRes as Response, next);
+    expect(mockRes.json).toBeCalledWith(expectedRes);
+    expect(mockRes.status).toBeCalledWith(400);
+  });
+
+  it("Must throw error extra props provided", () => {
+    const invalidBody = {
+      firstName: "Arthur",
+      lastName: "Morgan",
+      email: "arthur@email.com",
+      password: "testPassword",
+      confirmPassword: "testPassword",
+      role: "Admin",
+    };
+    mockReq = { body: invalidBody };
+    const testFunc = validateResource(createUserSchema);
+    testFunc(mockReq as Request, mockRes as Response, next);
+    expect(mockRes.status).toBeCalledWith(400);
+  });
+});
+
 describe("validate update password schema", () => {
   let mockReq: Partial<Request>;
   let mockRes: Partial<Response>;
